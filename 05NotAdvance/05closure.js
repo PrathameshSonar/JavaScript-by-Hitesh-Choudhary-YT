3aa16eb734be3b52c1e355c7c883d6f3c8181066
PrathameshSonar <PRATHAMESH@PRATHAMESH>
2021-01-19 12:09:27 +0530

    learnt about closure in JS

---
+++ 05NotAdvance/05closure.js
@@ -0,0 +1,24 @@
+// function init() {
+//     var fName = "Prathamesh";
+//     function sayFname() {
+//         console.log(fName);
+//     }
+    
+//     return sayFname;
+
+// }
+
+// var value = init();
+
+// value();
+
+function doAddition(x) {
+    return function(y) {
+        return x + y;
+    };
+}
+
+var add5 = doAddition(4);
+console.log(add5(4));
+
+console.log(doAddition(5)(10));
