65e5b28f7d90a74ee6b5b49a05d681846e67320b
PrathameshSonar <PRATHAMESH@PRATHAMESH>
2021-01-20 20:15:34 +0530

    strings in JS

---
+++ 06Advanceish/02strings.js
@@ -0,0 +1,9 @@
+var name = "PMS";
+var fullName = `${name} Sonar`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("r"));
+console.log(fullName.includes("ms"));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
