d20abda96b3f63a4538adda47d91f94b5ab20501
PrathameshSonar <PRATHAMESH@PRATHAMESH>
2021-01-21 11:49:32 +0530

    event loop

---
+++ 06Advanceish/08async.js
@@ -0,0 +1,18 @@
+const uno = () => {
+    console.log("One");
+};
+
+const dos = () => {
+    setTimeout(() => {
+        console.log("WOOOOOOhoooooooo ");
+    }, 3000);
+    console.log("Two");
+};
+
+const tres = () => {
+    console.log("Three");
+};
+
+uno();
+dos();
+tres();
