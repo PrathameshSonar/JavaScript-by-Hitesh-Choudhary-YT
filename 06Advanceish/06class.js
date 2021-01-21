9eb719ceca28028aea369b11cd9621e87234aae0
PrathameshSonar <PRATHAMESH@PRATHAMESH>
2021-01-21 11:33:53 +0530

    inheritance in js

--- 06Advanceish/06class.js
+++ 06Advanceish/06class.js
@@ -4,20 +4,50 @@ class User{
         this.email = email;
     }
 
-    courseList = [];
+    #courseList = [];
     getInfo() {
         return {name : this.name, email : this.email};
     }
 
     enrollCourse(name) {
-        this.courseList.push(name);
+        this.#courseList.push(name);
     }
 
     getCourseList() {
-        return this.courseList;
+        return this.#courseList;
     }
 
+    login(){
+        return "You are logged in";
+    }
+}
+
+class SubAdmin extends User{
+    constructor(name, email){
+        super(name, email);
+    }
+
+    getAdminInfo(){
+        return "I am subadmin";
+    }
+
+    login() {
+        return "Login for admin only";
+    }
 }
 
 
+
+
 module.exports = User;
+
+const rock = new User("ROCK", "rock.com");
+// console.log(rock.getInfo());
+rock.enrollCourse("Angular");
+// console.log(rock.getCourseList());
+// console.log(rock.courseList);
+
+const tom = new SubAdmin("tom", "tom.com");
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+console.log(tom.getInfo());
